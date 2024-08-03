55bbfcddbd37fcbc09eca31b2a3ac6a80961e309
Javier Orfo <javierorfo@prontonmail.com>
2024-08-02 22:39:28 -0300

    conditional on auditing

--- src/main/java/com/javi/auditory/AuditoryConfiguration.java
+++ src/main/java/com/javi/auditory/AuditoryConfiguration.java
@@ -1,5 +1,6 @@
 package com.javi.auditory;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -7,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@ConditionalOnProperty(value = "spring.auditory.mongo", havingValue = "false", matchIfMissing = true)
 public class AuditoryConfiguration {
     @Bean
     public AuditorAware<String> auditorAware() {
